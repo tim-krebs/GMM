844c191f97ea3acf4b9ccc49b5d31d068858e707
tim-krebs <timkrebs9@gmail.com>
2021-10-04 10:13:57 +0200

    added src folder

---
+++ src/my_lib.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+#include "my_lib.h"
+
+void print_hello_world()
+{
+    printf("Printf: Hello World\n");
+}
+
+void cout_hello_world()
+{
+    std::cout << "Cout: Hello World" << std::endl;
+}
